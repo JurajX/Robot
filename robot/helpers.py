added:  - calculation of the EE position w.r.t the origin  - simulator for the EE position fixed:  - missing asserts in the pendulum tests  - wrong potential energy in the pendulum tests  - bad config file parameters

robot/helpers.py
@@ -67,13 +67,15 @@ def plotLoss(loss, offset=0):
 
 
 def generateData(sim, cfg, set_size, batch_size, chunk_size=2**15):
-    """Generate random data from simulator.
+    """Generate random data from simulator for learning dynamic parameters.
     Arguments:
         sim        - simulator used to generate the data
         cfg        - configuration file for a robot
         set_size   - size of the dataset
         batch_size - batch size argument for data loader
         chunk_size - the data is generated in chunks of size chunk_size (default=2**15)
+    Returns:
+        torch.utils.data.DataLoader - containing the generated data
     """
     gravity_direction = generateUnitVectors([set_size, 3], sim.dtype)
     thetas = generateRandomVectors(set_size, cfg['q_min'], cfg['q_max'], sim.dtype)
@@ -87,6 +89,30 @@ def generateData(sim, cfg, set_size, batch_size, chunk_size=2**15):
             appliedTorques[b:e] = sim.getMotorTorque(thetas[b:e], dthetas[b:e], ddthetas[b:e], gravity_direction[b:e])
 
     dataset = torch.utils.data.TensorDataset(thetas, dthetas, ddthetas, appliedTorques, gravity_direction)
+    loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
+    return loader
+
+
+def generateDataKin(sim, cfg, set_size, batch_size, chunk_size=2**15):
+    """Generate random data from simulator for learning kinematic parameters.
+    Arguments:
+        sim        - simulator used to generate the data
+        cfg        - configuration file for a robot
+        set_size   - size of the dataset
+        batch_size - batch size argument for data loader
+        chunk_size - the data is generated in chunks of size chunk_size (default=2**15)
+    Returns:
+        torch.utils.data.DataLoader - containing the generated data
+    """
+    thetas = generateRandomVectors(set_size, cfg['q_min'], cfg['q_max'], sim.dtype)
+    coordinatesOfEE = torch.empty((set_size, cfg['dim']), dtype=sim.dtype)
+
+    with torch.no_grad():
+        for b in range(0, set_size, chunk_size):
+            e = b + chunk_size
+            coordinatesOfEE[b:e] = sim.getCoordinatesOfEE(thetas[b:e])
+
+    dataset = torch.utils.data.TensorDataset(thetas, coordinatesOfEE)
     loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=1)
     return loader
 
@@ -115,6 +141,43 @@ def learningLoop(agent, optimiser, loader, losses):
         optimiser.zero_grad()
         appliedTorque_est = agent.getMotorTorque(theta, dtheta, ddtheta, directionOfGravity=gravityDirection)
         loss = criterion(appliedTorque_est, appliedTorque)
+        loss.backward()
+        optimiser.step()
+
+        with torch.no_grad():
+            avg_loss = avg_loss + (loss.item() - avg_loss) / i
+            losses.append(loss.item())
+
+        if (i % tenthOfData == 0):
+            printRunningLoss(avg_loss, i // tenthOfData)
+    print("")
+    end = time.time()
+    print(f"elapsed time {(end-start):.2f}s.")
+    return avg_loss
+
+
+def learningLoopKin(agent, optimiser, loader, losses):
+    """Learning loop iterating over the whole dataset onece for the agent.
+    Arguments:
+        agent     - the learning agent
+        optimiser - optimiser used in learning; assumes that it contains the agent parameters
+        loader    - data loader containing the dataset
+        losses    - list where average batch loses are appended
+    Returns:
+        avg_loss  - average loss of the whole dataset
+    """
+    tenthOfData = len(loader.dataset) // (loader.batch_size * 10)
+    start = time.time()
+    criterion1 = torch.nn.MSELoss(reduction='mean')
+    criterion2 = torch.nn.L1Loss(reduction='mean')
+    avg_loss = 0.0
+    for i, data in enumerate(loader, 1):
+        theta = data[0].to(device=agent.device)
+        coordinatesOfEE = data[1].to(device=agent.device)
+
+        optimiser.zero_grad()
+        coordinatesOfEE_est = agent.getCoordinatesOfEE(theta)
+        loss = 0.5 * (criterion1(coordinatesOfEE_est, coordinatesOfEE) + criterion2(coordinatesOfEE_est, coordinatesOfEE))
         loss.backward()
         optimiser.step()
 

robot/robot.py
@@ -14,9 +14,9 @@ class Robot(torch.nn.Module):
         """Creates an instance of a Robot.
         Arguments:
             nLinks               - number of links (int)
-            directionOfGravity   - unit vector indicating the direction of the gravitational acceleration; iterable of shape (3)
+            directionOfGravity   - unit vector indicating the direction of the gravitational acceleration; iterable of shape (3, )
             rotationAxesOfJoints - coordinates of the pseudo-vectors parametrising the rotation axes of joints; iterable of shape (nLinks, 3)
-            frameCoordinates     - coordinates of the frame i relative to the frame i-1, i ∈ {1, ..., nLinks} ; iterable of shape (nLinks, 3)
+            frameCoordinates     - coordinates of the frame i w.r.t i-1, i ∈ {0, ..., nLinks} (i=-1 is the origin); iterable of shape (nLinks, 3)
             dtype                - type of underlying data; string, torch.float32 or torch.float64
         """
         super(Robot, self).__init__()
@@ -36,13 +36,14 @@ class Robot(torch.nn.Module):
 
         self.gravAccel = None
         self.setGravAccel(directionOfGravity)
-        tmp = self._makeTensor(rotationAxesOfJoints, shape=(self.nLinks, self.DIM))
-        tmp /= tmp.norm(dim=1, keepdim=True)
-        self.rotationAxesOfJoints = self._makeParameter(tmp, (self.nLinks, self.DIM))
-        tmp = torch.einsum('ni, ijk -> njk', self.rotationAxesOfJoints, self.SO3GEN)
-        self.L_rotationAxesOfJoints = self._makeParameter(tmp, (self.nLinks, self.DIM, self.DIM))
-        self.frameCoordinates = self._makeParameter(frameCoordinates, (self.nLinks, self.DIM))
-        self.triuFrameCoordinates = self._makeTriuFrameCoordinates()
+
+        self.rotationAxesOfJoints = None
+        self.L_rotationAxesOfJoints = None
+        self.setRotationAxesOfJoints(rotationAxesOfJoints)
+
+        self.frameCoordinates = None
+        self.triuFrameCoordinates = None
+        self.setFrameCoordinates(frameCoordinates)
 
         self.mass = self._makeRandnParameter(shape=(self.nLinks, ), std=1.0, requires_grad=True, abs=True)
         self.linkCoM = self._makeZeroParameter(shape=(self.nLinks, self.DIM), requires_grad=True)
@@ -204,15 +205,58 @@ class Robot(torch.nn.Module):
         shape = (self.linksXdim, self.nLinks)
         ones = torch.ones(self.DIM, self.nLinks, self.nLinks)
         tmp = torch.triu(ones, diagonal=1).transpose(0, 1).reshape(shape)
-        triuFrameCoordinates = tmp * self.frameCoordinates.reshape(self.linksXdim, 1)
+        triuFrameCoordinates = tmp * self.frameCoordinates[1:].reshape(self.linksXdim, 1)
         triuFrameCoordinates = self._makeParameter(triuFrameCoordinates, shape)
         return triuFrameCoordinates
+
+    def setRotationAxesOfJoints(self, rotationAxesOfJoints, requires_grad=False):
+        """Set custom parameter for rotation axes of joints and check if it has an appropriate shape.
+        Arguments:
+            directionOfGravity - value describing the direction of gravitational acceleration; iterable
+            requires_grad      - determines if the parameter is trainable; boolean (default=False)
+        Returns:
+            torch.nn.Parameter - containing gravitational acceleration vector with the norm equal to 9.80665
+        Raises:
+            ValueError         - the given directionOfGravity does not have the desired shape
+        """
+        shape_vec = (self.nLinks, self.DIM)
+        shape_mat = (self.nLinks, self.DIM, self.DIM)
+        tmp = self._makeTensor(rotationAxesOfJoints, shape=(self.nLinks, self.DIM))
+        tmp /= tmp.norm(dim=1, keepdim=True)
+        self.rotationAxesOfJoints = self._makeParameter(tmp, shape_vec, device=self.device, requires_grad=requires_grad)
+        tmp = torch.einsum('ni, ijk -> njk', self.rotationAxesOfJoints, self.SO3GEN)
+        self.L_rotationAxesOfJoints = self._makeParameter(tmp, shape_mat, device=self.device, requires_grad=False)
+
+    def normaliseRotationAxesOfJoints(self):
+        """Normalise the rotation axes of joints and recompute the associated Lie group generators.
+        Arguments:
+            None
+        Returns:
+            None
+        Raises:
+            None
+        """
+        self.rotationAxesOfJoints.data /= self.rotationAxesOfJoints.norm(dim=1, keepdim=True)
+        self.L_rotationAxesOfJoints.data = torch.einsum('ni, ijk -> njk', self.rotationAxesOfJoints, self.SO3GEN)
+
+    def setFrameCoordinates(self, frameCoordinates, requires_grad=False):
+        """Set a custom parameter for frame coordinates and check if it has an appropriate shape.
+        Arguments:
+            frameCoordinates   - coordinates of the frame i w.r.t i-1, i ∈ {0, ..., nLinks} (i=-1 is the origin); iterable of shape (nLinks, 3)
+            requires_grad      - determines if the parameter is trainable; boolean (default=False)
+        Returns:
+            torch.nn.Parameter - containing gravitational acceleration vector with the norm equal to 9.80665
+        Raises:
+            ValueError         - the given directionOfGravity does not have the desired shape
+        """
+        shape = (self.nLinks + 1, self.DIM)
+        self.frameCoordinates = self._makeParameter(frameCoordinates, shape, device=self.device, requires_grad=requires_grad)
+        self.triuFrameCoordinates = self._makeTriuFrameCoordinates()
 
     def setGravAccel(self, directionOfGravity, requires_grad=False):
         """Set a custom parameter for gravitational acceleration and check if it has an appropriate shape.
         Arguments:
             directionOfGravity - value describing the direction of gravitational acceleration; iterable
-            shape              - an iterable indicating the shape of the parameter
             requires_grad      - determines if the parameter is trainable; boolean (default=False)
         Returns:
             torch.nn.Parameter - containing gravitational acceleration vector with the norm equal to 9.80665
@@ -460,6 +504,7 @@ class Robot(torch.nn.Module):
         theta = self._makeTensor(theta, device=self.device)
         batch_size = theta.shape[0]
 
+        self.normaliseRotationAxesOfJoints()
         L_q = self.L_rotationAxesOfJoints.unsqueeze(0) * theta.view(batch_size, self.nLinks, 1, 1)
         Rot = torch.matrix_exp(L_q)
         dRot = self.L_rotationAxesOfJoints.matmul(Rot)
@@ -548,3 +593,27 @@ class Robot(torch.nn.Module):
         massMat, _, _, potEnergy = self._make_EoM_parameters(theta, directionOfGravity)
         kinEnergy = 0.5 * torch.einsum('bm, bmn, bn -> b', dtheta, massMat, dtheta)
         return kinEnergy - potEnergy
+
+    def getCoordinatesOfEE(self, theta):
+        """
+        Compute the position of the robot's end effector w.r.t the origin.
+        Arguments:
+            theta              - joint angles; iterable of shape (batch_size, nLinks)
+        Returns:
+            torch.Tensor       - containing the computed end effector position
+        """
+        theta = self._makeTensor(theta, device=self.device)
+        batch_size = theta.shape[0]
+
+        rotAxes = self.rotationAxesOfJoints / self.rotationAxesOfJoints.norm(dim=1, keepdim=True)
+        L_rotAxes = torch.einsum('ni, ijk -> njk', rotAxes, self.SO3GEN)
+        L_q = L_rotAxes.unsqueeze(0) * theta.view(batch_size, self.nLinks, 1, 1)
+        Rot = torch.matrix_exp(L_q)
+
+        eePosition = self.frameCoordinates[0].unsqueeze(0).repeat(batch_size, 1)
+        tmp = torch.eye(self.DIM, dtype=self.dtype).unsqueeze(0)
+        for i in range(self.nLinks):
+            tmp = tmp.matmul(Rot[:, i])
+            eePosition += tmp.matmul(self.frameCoordinates[i + 1])
+
+        return eePosition

robot/simulator.py
@@ -1,9 +1,10 @@
 import argparse
 
-import helpers as hlp
-import robot as robot
 import torch
 import yaml
+
+import helpers as hlp
+import robot as robot
 
 
 def main(init):
@@ -38,7 +39,7 @@ def main(init):
 
     for average, beta, lr in zip(averages, betas, lrs):
         avg_loss = 1.
-        print(f"target average loss: {average:.0e}")
+        print(f"----- target average loss: {average:.0e}")
         optimiser = torch.optim.Adam(rbt.parameters(), lr=lr, betas=beta, eps=1e-08, weight_decay=0, amsgrad=True)
         while (avg_loss > average):
             avg_loss = hlp.learningLoop(rbt, optimiser, loader, losses)

tests/test_pendulum.py
@@ -21,9 +21,10 @@ for key, value in cfg_d.items():
 class Test_SinglePendulum():
 
     def test_mathematical(self):
+        """The Robot class should correctly compute Lagrangian for the a mathematical pendulum."""
         sim = robot.Robot(cfg_s['n_links'], cfg_s['direction'], cfg_s['rot_axes'], cfg_s['f_coos'], cfg_s['dtype'])
         sim.setInertialParams(cfg_s['mass'], cfg_s['com'], cfg_s['p_inertia'], cfg_s['rot_of_p_axes'], cfg_s['damping'])
-        sim.triangle.data = torch.zeros_like(sim.triangle.data)
+        sim.triangle.data = torch.zeros_like(sim.triangle.data)    # Mathematical pendulum => no inertia tensor
 
         thetas = hlp.generateRandomVectors(cfg_s['set_size'], [-cfg_s['pi']], [cfg_s['pi']], cfg_s['dtype'])
         dthetas = hlp.generateRandomVectors(cfg_s['set_size'], [-cfg_s['max_vel']], [cfg_s['max_vel']], cfg_s['dtype'])
@@ -32,24 +33,26 @@ class Test_SinglePendulum():
         V = cfg_s['mass'][0] * cfg_s['g'] * cfg_s['com'].norm() * (-1) * torch.cos(thetas.squeeze())
         expected = T - V
         returned = sim.getLagrangian(thetas, dthetas)
-        expected.allclose(returned)
+        assert expected.allclose(returned)
 
     def test_physical_com(self):
+        """The Robot class should correctly compute Lagrangian for the a physical pendulum rotating around it's CoM."""
         sim = robot.Robot(cfg_s['n_links'], cfg_s['direction'], cfg_s['rot_axes'], cfg_s['f_coos'], cfg_s['dtype'])
         sim.setInertialParams(cfg_s['mass'], cfg_s['com'], cfg_s['p_inertia'], cfg_s['rot_of_p_axes'], cfg_s['damping'])
-        sim.triangle.data = torch.zeros_like(sim.triangle.data)
+        sim.mass.data = torch.zeros_like(sim.mass.data)    # rotation around CoM => no potential energy
 
         thetas = hlp.generateRandomVectors(cfg_s['set_size'], [-cfg_s['pi']], [cfg_s['pi']], cfg_s['dtype'])
         dthetas = hlp.generateRandomVectors(cfg_s['set_size'], [-cfg_s['max_vel']], [cfg_s['max_vel']], cfg_s['dtype'])
-        projectedInertia = torch.einsum('ni, nij, nj -> ', cfg_s['rot_axes'], sim.inertia, cfg_s['rot_axes'])
 
+        projectedInertia = torch.einsum('ni, nij, nj -> ', cfg_s['rot_axes'], sim.inertia, cfg_s['rot_axes'])
         T = 0.5 * projectedInertia * dthetas.squeeze().pow(2)
-        V = cfg_s['mass'][0] * cfg_s['g'] * cfg_s['com'].norm() * (-1) * torch.cos(thetas.squeeze())
+        V = torch.zeros_like(T)    # rotation around CoM => no potential energy
         expected = T - V
         returned = sim.getLagrangian(thetas, dthetas)
-        expected.allclose(returned)
+        assert expected.allclose(returned)
 
     def test_physical(self):
+        """The Robot class should correctly compute Lagrangian for the a physical pendulum."""
         sim = robot.Robot(cfg_s['n_links'], cfg_s['direction'], cfg_s['rot_axes'], cfg_s['f_coos'], cfg_s['dtype'])
         sim.setInertialParams(cfg_s['mass'], cfg_s['com'], cfg_s['p_inertia'], cfg_s['rot_of_p_axes'], cfg_s['damping'])
 
@@ -58,16 +61,16 @@ class Test_SinglePendulum():
 
         projectedInertia = torch.einsum('ni, nij, nj -> ', cfg_s['rot_axes'], sim.inertia, cfg_s['rot_axes'])
         T = 0.5 * cfg_s['mass'][0] * (cfg_s['com'].norm() * dthetas.squeeze()).pow(2) + 0.5 * projectedInertia * dthetas.squeeze().pow(2)
-        V = cfg_s['mass'][0] * cfg_s['g'] * cfg_s['com'].norm() * (-torch.cos(thetas.squeeze()))
+        V = cfg_s['mass'][0] * cfg_s['g'] * cfg_s['com'].norm() * (-1) * torch.cos(thetas.squeeze())
         expected = T - V
         returned = sim.getLagrangian(thetas, dthetas)
-        expected.allclose(returned)
+        assert expected.allclose(returned)
 
 
 class Test_DoublePendulum():
 
     def test_mathematical(self):
-        # =========== Double Mathematical Pendulum ===========
+        """The Robot class should correctly compute Lagrangian for the a double mathematical pendulum."""
         sim = robot.Robot(cfg_d['n_links'], cfg_d['direction'], cfg_d['rot_axes_planar'], cfg_d['f_coos_m'], cfg_d['dtype'])
         sim.setInertialParams(cfg_d['mass'], cfg_d['com'], cfg_d['p_inertias_math'], cfg_d['rot_of_p_axes'], cfg_d['damping'])
         sim.triangle.data = torch.zeros_like(sim.triangle.data)
@@ -88,10 +91,10 @@ class Test_DoublePendulum():
 
         expected = T - V1 - V2
         returned = sim.getLagrangian(thetas, dthetas)
-        expected.allclose(returned)
+        assert expected.allclose(returned)
 
     def test_physical_planar(self):
-        # =========== Double Physical Pendulum ===========
+        """The Robot class should correctly compute Lagrangian for the a double physical pendulum restricted to a plane."""
         sim = robot.Robot(cfg_d['n_links'], cfg_d['direction'], cfg_d['rot_axes_planar'], cfg_d['f_coos_p'], cfg_d['dtype'])
         sim.setInertialParams(cfg_d['mass'], cfg_d['com'], cfg_d['p_inertias_phys'], cfg_d['rot_of_p_axes'], cfg_d['damping'])
 
@@ -99,25 +102,25 @@ class Test_DoublePendulum():
         dthetas = hlp.generateRandomVectors(cfg_d['set_size'], [-cfg_d['max_vel']] * 2, [cfg_d['max_vel']] * 2, cfg_d['dtype'])
 
         V1 = cfg_d['mass'][0] * cfg_d['g'] * cfg_d['com'][0].norm() * (-torch.cos(thetas[:, 0]))
-        V2 = cfg_d['mass'][1] * cfg_d['g'] * cfg_d['f_coos_p'][0].norm() * (-torch.cos(thetas[:, 0]))
+        V2 = cfg_d['mass'][1] * cfg_d['g'] * cfg_d['f_coos_p'][0 + 1].norm() * (-torch.cos(thetas[:, 0]))
         V2 += cfg_d['mass'][1] * cfg_d['g'] * cfg_d['com'][1].norm() * (-torch.cos(thetas[:, 0] + thetas[:, 1]))
 
         projectedInertia = torch.einsum('ni, nij, nj -> n', cfg_d['rot_axes_planar'], sim.inertia, cfg_d['rot_axes_planar'])
         T1 = 0.5 * projectedInertia[0] * dthetas[:, 0].pow(2) + 0.5 * projectedInertia[1] * dthetas.sum(dim=1).pow(2)
-        tmp11 = cfg_d['f_coos_p'][0].norm().pow(2) + cfg_d['com'][1].norm().pow(2)
-        tmp12 = 2 * cfg_d['f_coos_p'][0].norm() * cfg_d['com'][1].norm() * torch.cos(thetas[:, 1])
+        tmp11 = cfg_d['f_coos_p'][0 + 1].norm().pow(2) + cfg_d['com'][1].norm().pow(2)
+        tmp12 = 2 * cfg_d['f_coos_p'][0 + 1].norm() * cfg_d['com'][1].norm() * torch.cos(thetas[:, 1])
         tmp1 = cfg_d['mass'][1] * (tmp11+tmp12)
         tmp1 = tmp1 + cfg_d['mass'][0] * cfg_d['com'][0].norm().pow(2)
         tmp2 = cfg_d['mass'][1] * cfg_d['com'][1].norm().pow(2)
-        tmp3 = tmp2 + cfg_d['mass'][1] * cfg_d['f_coos_p'][0].norm() * cfg_d['com'][1].norm() * torch.cos(thetas[:, 1])
+        tmp3 = tmp2 + cfg_d['mass'][1] * cfg_d['f_coos_p'][0 + 1].norm() * cfg_d['com'][1].norm() * torch.cos(thetas[:, 1])
         T2 = 0.5 * (tmp1 * dthetas[:, 0].pow(2) + tmp2 * dthetas[:, 1].pow(2) + 2 * tmp3 * dthetas[:, 0] * dthetas[:, 1])
 
         expected = T1 + T2 - V1 - V2
         returned = sim.getLagrangian(thetas, dthetas)
-        expected.allclose(returned)
+        assert expected.allclose(returned)
 
     def test_physical_non_planar(self):
-        # =========== Double Non-Planar Physical Pendulum ===========
+        """The Robot class should correctly compute Lagrangian for the a double physical pendulum not restricted to a plane."""
         sim = robot.Robot(cfg_d['n_links'], cfg_d['direction'], cfg_d['rot_axes_non_planar'], cfg_d['f_coos_p'], cfg_d['dtype'])
         sim.setInertialParams(cfg_d['mass'], cfg_d['com'], cfg_d['p_inertias_phys'], cfg_d['rot_of_p_axes'], cfg_d['damping'])
 
@@ -125,7 +128,7 @@ class Test_DoublePendulum():
         dthetas = hlp.generateRandomVectors(cfg_d['set_size'], [-cfg_d['max_vel']] * 2, [cfg_d['max_vel']] * 2, cfg_d['dtype'])
 
         V1 = cfg_d['mass'][0] * cfg_d['g'] * cfg_d['com'][0].norm() * (-torch.cos(thetas[:, 0]))
-        V2 = cfg_d['mass'][1] * cfg_d['g'] * cfg_d['f_coos_p'][0].norm() * (-torch.cos(thetas[:, 0]))
+        V2 = cfg_d['mass'][1] * cfg_d['g'] * cfg_d['f_coos_p'][0 + 1].norm() * (-torch.cos(thetas[:, 0]))
         V3 = cfg_d['mass'][1] * cfg_d['g'] * cfg_d['com'][1].norm() * (-torch.cos(thetas[:, 0]) * torch.cos(thetas[:, 1]))
 
         Lq = torch.einsum('ijk, ni -> njk', sim.SO3GEN, cfg_d['rot_axes_non_planar']).unsqueeze(0) * thetas.unsqueeze(-1).unsqueeze(-1)
@@ -143,10 +146,10 @@ class Test_DoublePendulum():
         Inertia[:, 1, 1] = sim.inertia[1].matmul(cfg_d['rot_axes_non_planar'][1]).matmul(cfg_d['rot_axes_non_planar'][1])
         T1 = 0.5 * torch.einsum('bn, bnm, bm -> b', dthetas, Inertia, dthetas)
         tmp1 = cfg_d['mass'][0] * cfg_d['com'][0].norm().pow(2)
-        tmp1 = tmp1 + cfg_d['mass'][1] * (cfg_d['f_coos_p'][0].norm() + cfg_d['com'][1].norm() * torch.cos(thetas[:, 1])).pow(2)
+        tmp1 = tmp1 + cfg_d['mass'][1] * (cfg_d['f_coos_p'][0 + 1].norm() + cfg_d['com'][1].norm() * torch.cos(thetas[:, 1])).pow(2)
         tmp2 = cfg_d['mass'][1] * cfg_d['com'][1].norm().pow(2)
         T2 = 0.5 * (tmp1 * dthetas[:, 0].pow(2) + tmp2 * dthetas[:, 1].pow(2))
 
         expected = T1 + T2 - V1 - V2 - V3
         returned = sim.getLagrangian(thetas, dthetas)
-        expected.allclose(returned)
+        assert expected.allclose(returned)

tests/test_robot_compute.py
@@ -152,7 +152,7 @@ class Test_Computation():
                 elif r == c:
                     tmp[r, :, c] = param_robot.linkCoM[r]
                 else:
-                    tmp[r, :, c] = param_robot.frameCoordinates[r]
+                    tmp[r, :, c] = param_robot.frameCoordinates[r + 1]     # disregard the coos of the first joint w.r.t the origin (frameCoordinates[0])
         tmp = tmp.reshape(linksXdim, nLinks)
 
         expected = bigRot.matmul(tmp).reshape(bSize, nLinks + 1, nLinks, DIM, nLinks)
@@ -304,3 +304,44 @@ class Test_Computation():
             assert returned.allclose(expected)
         else:
             assert returned.allclose(expected, rtol=1e-02, atol=1e-02)
+
+    def test_coordinatesOfEE(self, param_robot, thetas=cfg['thetas']):
+        """The getCoordinatesOfEE member function should return the correct position of the end effector."""
+        nLinks = param_robot.nLinks
+        dtype = param_robot.dtype
+        device = param_robot.device
+        frameCoordinates = param_robot.frameCoordinates
+        theta = thetas[:nLinks].unsqueeze(0).to(dtype=dtype)
+        theta = theta.to(device=device)
+        batch_size = theta.shape[0]
+
+        L_rotAxes = torch.einsum('ni, ijk -> njk', param_robot.rotationAxesOfJoints, param_robot.SO3GEN)
+        L_q = L_rotAxes.unsqueeze(0) * theta.view(batch_size, nLinks, 1, 1)
+        Rot = torch.matrix_exp(L_q)
+
+        scannedRot = torch.empty_like(Rot)
+        scannedRot[:, 0] = Rot[:, 0]
+        for i in range(1, nLinks):
+            scannedRot[:, i] = scannedRot[:, i - 1].bmm(Rot[:, i])
+
+        expected = frameCoordinates[0] + torch.einsum('bnij, nj -> bi', scannedRot, frameCoordinates[1:])
+        returned = param_robot.getCoordinatesOfEE(theta)
+
+        if dtype == torch.float64:
+            assert returned.allclose(expected)
+        else:
+            assert returned.allclose(expected, rtol=1e-02, atol=1e-02)
+
+    def test_defaultEE(self, param_robot):
+        """The getCoordinatesOfEE member function should return sum of the frame coordiantes as the end effector position when
+        the robot is in the home position (zero angles)."""
+        nLinks = param_robot.nLinks
+        batch_size = 1
+        dtype = param_robot.dtype
+        device = param_robot.device
+        frameCoordinates = param_robot.frameCoordinates
+        theta = torch.zeros((batch_size, nLinks), dtype=dtype, device=device)
+
+        expected = frameCoordinates.sum(dim=0)
+        returned = param_robot.getCoordinatesOfEE(theta)
+        assert returned.allclose(expected)

tests/test_robot_init.py
@@ -172,14 +172,14 @@ class Test_RobotInitialisers():
         nLinks = args[0]
         DIM = args[1]
         linksXdim = nLinks * DIM
-        frameCoordinates = torch.nn.Parameter(torch.rand(nLinks, DIM), requires_grad=False)
+        frameCoordinates = torch.nn.Parameter(torch.rand(nLinks + 1, DIM), requires_grad=False)
         monkeypatch.setattr(cls_robot, 'nLinks', nLinks, raising=True)
         monkeypatch.setattr(cls_robot, 'DIM', DIM, raising=True)
         monkeypatch.setattr(cls_robot, 'linksXdim', linksXdim, raising=True)
         monkeypatch.setattr(cls_robot, 'frameCoordinates', frameCoordinates, raising=True)
 
         tmp = torch.triu(torch.ones(DIM, nLinks, nLinks), diagonal=1).transpose(0, 1).reshape(linksXdim, nLinks)
-        expected = tmp * cls_robot.frameCoordinates.reshape(linksXdim, 1)
+        expected = tmp * cls_robot.frameCoordinates[1:].reshape(linksXdim, 1)  # do not include the fram from origin to the first joint
 
         returned = cls_robot._makeTriuFrameCoordinates()
         assert type(returned) is torch.nn.parameter.Parameter